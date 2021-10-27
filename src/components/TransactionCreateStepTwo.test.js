6237fd2d0e68f1d4a08b777a71b7560baa703cdb
mark <markpackham1@gmail.com>
2021-10-27 16:03:59 +0100

    test pay button disabled
    https://youtu.be/OVNjsIto9xM?t=1154

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,7 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
-test("on initial render, the pay button is disabled", () => {
-  render(<TransactionCreateStepTwo />);
-  screen.debug();
+test("on initial render, the pay button is disabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+
+  expect(await screen.findByRole(await "button", { name: /pay/i })).toBeDisabled();
 });
